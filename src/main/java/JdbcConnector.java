46188fda4c8cc83c793286073e15e90405680787
TheirryEHB <thierry.klougbo@student.ehb.be>
2022-03-21 15:08:22 +0100

    databse conectie fix

--- src/main/java/JdbcConnector.java
+++ src/main/java/JdbcConnector.java
@@ -8,10 +8,10 @@ public class JdbcConnector {
 
     public static Connection makeConnection(){
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.
-                    getConnection("jdbc:oracle:thin:@10.3.56.3:8002/frontend"
-                            ," frontend","hH6QbNdqeVxKTM23uW3EvrS7RgdP7CLZ");
+                    getConnection("jdbc:mysql://10.3.56.3:5002/frontend"
+                            ,"frontend","hH6QbNdqeVxKTM23uW3EvrS7RgdP7CLZ");
             Statement stmt = con.createStatement();
             createTables(stmt);
             System.out.println("Created DB Connection....");
@@ -25,9 +25,10 @@ public class JdbcConnector {
     }
 
     private static void createTables(Statement stmt) throws SQLException{
-        String sqlCreate = "CREATE TABLE IF NOT EXIST " + "RegisteredUser"
-                + "name Varchar(225)"
-                + "email Varchar(225)";
+        String sqlCreate = "CREATE TABLE IF NOT EXISTS RegisteredUser "
+                + "(id INTEGER not NULL, " +
+                " username Varchar(225), "
+                + " email Varchar(225))";
         stmt.execute(sqlCreate);
     }
 }
